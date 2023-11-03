7c822b7845304594e0f5af56299028c82292bcd0
Ataliba Miguel <112531858+achilela@users.noreply.github.com>
2023-11-03 22:35:34 +0100

    Update Andro_GPT_Llama2.py

--- Andro_GPT_Llama2.py
+++ Andro_GPT_Llama2.py
@@ -135,8 +135,8 @@ def select_llm() -> Union[ChatOpenAI, LlamaCpp]:
     
     elif model_name.startswith("llama-2-"):
 	    
-	    API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-hf"
-            headers = {"Authorization": "Bearer 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"}
+	API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-hf"
+        headers = {"Authorization": "Bearer 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"}
 	    
         callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
         
