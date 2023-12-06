819be264baa6f40c5d7b69b8ba810a63063b864d
Ataliba Miguel <112531858+achilela@users.noreply.github.com>
2023-12-06 21:44:08 +0100

    Update Andro_GPT_Llama2.py

--- Andro_GPT_Llama2.py
+++ Andro_GPT_Llama2.py
@@ -86,9 +86,9 @@ def select_llm() -> Union[ChatOpenAI, LlamaCpp]:
                                 temperature=temperature)
         
 
-openai_api_key = st.secrets["openai_key"]
+#openai_api_key = st.secrets["openai_key"]
 #openai_api_key = os.environ[OPENAI_API_KEY]
-#openai_api_key = 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
+openai_api_key = "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"
 
 
 def configure_qa_chain(uploaded_files):
