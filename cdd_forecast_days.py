ccc8186d2d52a647d97f9fb29832b6638872e897
Ataliba Paulo Chilela Miguel <112531858+achilela@users.noreply.github.com>
2024-05-12 12:34:01 +0100

    Update cdd_forecast_days.py

--- cdd_forecast_days.py
+++ cdd_forecast_days.py
@@ -100,6 +100,7 @@ col1, col2 = st.columns(2)
 
 with col2:
     end_time = st.time_input("Select the time on the future date:", value=datetime.now().time())
+    end_date = reamaining_days
     if st.button("Start Countdown"):
         text_placeholder = st.empty()
         countdown_timer(end_date, end_time, text_placeholder)
